8c289d74255af64d94d5a13b1228ac139b14b962
Owen Dugan <owen@duganhome.com>
2024-10-13 14:13:48 -0700

    Create prepare.py

---
+++ data/half_random_tokens/prepare.py
@@ -0,0 +1,112 @@
+# saves the openwebtext dataset to a binary file for training. following was helpful:
+# https://github.com/HazyResearch/flash-attention/blob/main/training/src/datamodules/language_modeling_hf.py
+
+import os
+import random
+import string
+import torch
+from tqdm import tqdm
+import numpy as np
+import tiktoken
+from datasets import load_dataset # huggingface datasets
+
+# number of workers in .map() call
+# good number to use is ~order number of cpu cores // 2
+num_proc = 8
+
+# number of workers in load_dataset() call
+# best number might be different from num_proc above as it also depends on NW speed.
+# it is better than 1 usually though
+num_proc_load_dataset = num_proc
+
+if __name__ == '__main__':
+    # takes 54GB in huggingface .cache dir, about 8M documents (8,013,769)
+    dataset = load_dataset("openwebtext", num_proc=num_proc_load_dataset)
+
+    train_portion = 0.01
+
+    # Create a smaller train dataset and a validation set
+    full_train = dataset["train"]
+    train_size = int(len(full_train) * train_portion)
+    
+    # Split the dataset
+    split_dataset = full_train.train_test_split(
+        train_size=train_size,
+        test_size=0.0005,  # This creates a small validation set
+        seed=2357,
+        shuffle=True
+    )
+    split_dataset['val'] = split_dataset.pop('test') # rename the test split to val
+
+    # this results in:
+    # >>> split_dataset
+    # DatasetDict({
+    #     train: Dataset({
+    #         features: ['text'],
+    #         num_rows: 8009762
+    #     })
+    #     val: Dataset({
+    #         features: ['text'],
+    #         num_rows: 4007
+    #     })
+    # })
+
+    # we now want to tokenize the dataset. first define the encoding function (gpt2 bpe)
+    enc = tiktoken.get_encoding("gpt2")
+    def process(example):
+        if random.choice([True, False]):
+            example = "|START|" + example["text"]
+        else:
+            # generate random text of the same length as the example
+            example = "".join(np.random.choice(list(string.printable), len(example["text"])))
+
+        char_tokens = [ord(char) for char in example]
+        ids = enc.encode_ordinary(example) # encode_ordinary ignores any special tokens
+        token_starts = enc.decode_with_offsets(ids)[1]
+
+        token_start_tensor = torch.zeros(len(char_tokens), dtype=torch.int)
+        token_start_tensor[token_starts] = 1
+        token_start_tensor = token_start_tensor.tolist()
+        #token_start_tensor = [1 if i in token_starts else 0 for i in range(len(char_tokens))]
+
+        # note: I think eot should be prepended not appended... hmm. it's called "eot" though...
+        assert len(char_tokens) == len(token_start_tensor)
+        out = {'char_tokens': char_tokens, 'token_starts': token_start_tensor, 'len': len(char_tokens)}
+        return out
+
+    # tokenize the dataset
+    tokenized = split_dataset.map(
+        process,
+        remove_columns=['text'],
+        desc="tokenizing the splits",
+        num_proc=num_proc,
+    )
+
+    # concatenate all the ids in each dataset into one large file we can use for training
+    for split, dset in tokenized.items():
+        arr_len = np.sum(dset['len'], dtype=np.uint64)
+        filename = os.path.join(os.path.dirname(__file__), f'{split}.bin')
+        dtype = np.uint16 # (can do since enc.max_token_value == 50256 is < 2**16)
+        in_arr = np.memmap(filename, dtype=dtype, mode='w+', shape=(arr_len,))
+        out_arr = np.memmap(filename.replace('.bin', '_token_starts.bin'), dtype=np.uint8, mode='w+', shape=(arr_len,))
+        total_batches = 1024
+
+        idx = 0
+        for batch_idx in tqdm(range(total_batches), desc=f'writing {filename}'):
+            # Batch together samples for faster write
+            batch = dset.shard(num_shards=total_batches, index=batch_idx, contiguous=True).with_format('numpy')
+            in_arr_batch = np.concatenate(batch['char_tokens'])
+            out_arr_batch = np.concatenate(batch['token_starts'])
+            # Write into mmap
+            in_arr[idx : idx + len(in_arr_batch)] = in_arr_batch
+            out_arr[idx : idx + len(out_arr_batch)] = out_arr_batch
+            idx += len(in_arr_batch)
+        in_arr.flush()
+        out_arr.flush()
+
+    # train.bin is ~17GB, val.bin ~8.5MB
+    # train has ~9B tokens (9,035,582,198)
+    # val has ~4M tokens (4,434,897)
+
+    # to read the bin files later, e.g. with numpy:
+    # m = np.memmap('train.bin', dtype=np.uint16, mode='r')
