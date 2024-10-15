cbda27890257e6374b19769311c0e41223af446f
Owen Dugan <owen@duganhome.com>
2024-10-15 16:27:52 -0700

    Update model_token_predicter.py

--- model_token_predicter.py
+++ model_token_predicter.py
@@ -184,6 +184,8 @@ class GPT_token_predictor(nn.Module):
         if targets is not None:
             # if we are given some desired targets also calculate the loss
             logits = self.lm_head(x)
+            print(torch.min(targets))
+            print(torch.max(targets))
             loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
         else:
             # inference-time mini-optimization: only forward the lm_head on the very last position
